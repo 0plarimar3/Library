056e66a7ce6d64c161257a58eae67a07ad4a70a6
ITACHI-op-21 <mayankjainvis@gmail.com>
2021-12-29 12:55:30 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+favLang = {}
+a = input("Enter your favorite language M.J\n")
+b = input("Enter your favorite language J.T\n")
+c = input("Enter your favorite language Y.K\n")
+favLang['M.J'] = a
+favLang['J.T'] = b
+favLang['Y.K'] = c
+print(favLang)
